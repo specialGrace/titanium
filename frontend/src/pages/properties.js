6b24179f4bd50a05c8c44d87e1aad7b88e4dd52e
specialGrace <tunmiseadebayo9@gmail.com>
2023-03-02 06:28:13 +0100

    update

--- frontend/src/pages/properties.js
+++ frontend/src/pages/properties.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Layout from '../components/Layout'
+import Layout from '../components/Properties/Properties'
 import Properties from '../components/Properties/Properties'
 
 function properties() {
